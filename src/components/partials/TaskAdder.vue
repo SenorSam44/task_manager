a8b5c71fe3eb0188e9ef230e405c6849ac5dc139
Sadman Naib <samsikder47@gmail.com>
2021-03-16 19:54:04 +0600

    modal ui update

--- src/components/partials/TaskAdder.vue
+++ src/components/partials/TaskAdder.vue
@@ -2,22 +2,22 @@
   <div class="modal" v-if="showModal">
     <form @submit="onSubmit" class="add-form">
       <div class="form-control">
-        <label>Task name
-          <input type="text" v-model="name" placeholder="Task Name" name="name"/>
-        </label>
+        <label for="name">Task name </label>
+          <input type="text" v-model="name" placeholder="Task Name" id="name" name="name"/>
+
       </div>
       <div class="form-control">
-        <label>Date
-          <input type="date" v-model="date" placeholder="Task Date" name="date"/>
-        </label>
+        <label for="date">Date</label>
+          <input type="date" v-model="date" placeholder="Task Date" name="date" id="date"/>
+
       </div>
       <div class="form-control">
-        <label>Set Reminder
-          <input type="checkbox" v-model="reminder" name="reminder"/>
+        <label style="padding: 10px" for="reminder">Set Reminder
+          <input style="display: inline-block; width: auto" type="checkbox" v-model="reminder" id="reminder" name="reminder"/>
         </label>
       </div>
 
-      <button type="submit">Submit</button>
+      <button class="btn btn-primary" type="submit">Submit</button>
     </form>
   </div>
 </template>
@@ -62,6 +62,7 @@ export default {
 <style scoped>
   .add-form{
     margin: 20px;
+    display: block;
   }
   .modal {
     position: fixed;
@@ -70,7 +71,7 @@ export default {
     transform: translate(-50%, -50%);
     width: 600px;
     max-width: 100%;
-    height: 400px;
+    height: max-content;
     max-height: 100%;
     background: #FFFFFF;
     box-shadow: 2px 2px 20px 1px;
@@ -78,5 +79,26 @@ export default {
     display: flex;
     border-radius: 10px;
     flex-direction: column;
+  }
+
+  .form-control{
+    margin: 10px;
+  }
+  .form-control label{
+    display: block;
+    width: 100%;
+    text-align: left;
+    padding-bottom: 10px;
+  }
+  .form-control input{
+    display: block;
+    width: 100%;
+  }
+
+  button{
+    display: flex;
+    float: right;
+    margin-top: 15px;
+    margin-right: 15px;
   }
 </style>
